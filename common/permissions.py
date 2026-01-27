005b2f9caab436941e877c57392600b267a52d37
anselmlys <anselm.lisa@gmail.com>
2026-01-27 15:46:18 +0100

    fix: IsProjectContributor permission now also work if object to access is an Issue or a Comment

--- common/permissions.py
+++ common/permissions.py
@@ -10,10 +10,30 @@ class IsAdminAuthenticated(BasePermission):
 
 
 class IsProjectContributor(BasePermission):
-    '''Give access to project contributor.'''
+    '''
+    Give access to project contributor.
+    Can only give acess to objects which are either Project, Issue or Comment.
+    '''
     
     def has_object_permission(self, request, view, obj):
-        return Contributor.objects.filter(project=obj, user=request.user).exists()
+        user = request.user
+
+        # Retrieve the project if obj is an Issue
+        project = getattr(obj, 'project', None)
+
+        # Check if obj is the project
+        if project is None and obj.__class__.__name__ == "Project":
+            project = obj
+
+        # Retrieve the project if obj is a Comment
+        if project is None and hasattr(obj, "issue"):
+            project = obj.issue.project
+
+        # Do not give access if obj is not Project, Issue or Comment
+        if project is None:
+            return False
+
+        return Contributor.objects.filter(project=project, user=user).exists()
 
 
 class IsAuthor(BasePermission):
